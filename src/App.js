a776e1d2060b1d83a93c6165cb86f916a8c35258
Yatt0Ikigai <minaster2312@gmail.com>
2022-05-21 20:31:09 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"h-screen w-screen flex flex-col justify-center items-center"}>
+        <h1 className={"text-7xl animate-bounce"}>PATH FINDER</h1>
+        <p className={"text-xl"}>Press &gt; to open search menu</p>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {PlatformProvider} from "./PlatformContext"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RouteMap from "./RouteMap"
+import "./index.css"
+import Sidebar from "./Sidebar";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <PlatformProvider>
+            <BrowserRouter>
+                <Sidebar/>
+                <Routes>
+                    <Route path={"/"} element={<App/>}/>
+                    <Route path={"/map"} element={<RouteMap/>}/>
+                </Routes>
+            </BrowserRouter>
+        </PlatformProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
